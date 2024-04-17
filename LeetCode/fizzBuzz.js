d24e04fbe3bdafc1fa01fee0dca75cfae29c1a5d
Micheal Burnett <80649997+burnett-micheal@users.noreply.github.com>
2024-04-17 14:52:16 -0600

    Create fizzBuzz.js

---
+++ LeetCode/fizzBuzz.js
@@ -0,0 +1,28 @@
+// Link: https://leetcode.com/problems/fizz-buzz
+
+var fizzBuzz = function(n) {
+    const getFizzBuzz = (num) => {
+        // By using phrases, we can add many more number - string combinations by just adding them to the phrases object.
+        let phrases = {3: "Fizz", 5: "Buzz"};
+
+        let result = '';
+        for (let dividend in phrases) {
+            if (num % dividend === 0) {
+                result += phrases[dividend];
+            }
+        }
+
+        if (result === '') {
+            result = `${num}`;
+        }
+
+        return result;
+    }
+
+    const resultArr = [];
+    for(let i = 1; i <= n; i++) {
+        resultArr.push(getFizzBuzz(i));
+    }
+
+    return resultArr;
+};
